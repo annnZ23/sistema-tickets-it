00994e3d125a1cc3d40d30f48febc093e5246825
Ana Zepeda <anacristelzepeda09b@gmail.com>
2026-06-17 16:04:12 -0600

    feat: avanceuno

--- backend/routes/auth.routers.js
+++ backend/routes/auth.routers.js
@@ -1,36 +1,26 @@
 const prisma = require("../lib/prisma");
 const bcrypt = require("bcrypt");
-
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
-
-    // 🔍 Buscar usuario
     const user = await prisma.user.findUnique({
       where: { email }
     });
-
     if (!user) {
       return res.status(404).json({
         message: "Usuario no encontrado"
       });
     }
-
-    // 🔐 Comparar contraseña
     const valid = await bcrypt.compare(password, user.password);
-
     if (!valid) {
       return res.status(401).json({
         message: "Contraseña incorrecta"
       });
     }
-
-    // ✅ Login correcto
     res.json({
       message: "Login exitoso",
       user
     });
-
   } catch (error) {
     console.error(error);
     res.status(500).json({
